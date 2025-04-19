77566b7134906ad872697b1e05844525d6dfa5bb
sudatra <mail2sudatra@gmail.com>
2025-04-19 11:31:14 +0530

    axis control

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
+    [SerializeField] float moveSpeed = 10f;
 
     void Start() {
         
     }
 
     void Update() {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float yValue = 0f;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
         transform.Translate(xValue, yValue, zValue);
     }
 }
